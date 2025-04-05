611ee9261a27c3d92e27d2e36222ebab6aaaec07
nothero111 <3599545378@qq.com>
2025-04-05 12:46:02 +0800

    完成了屏幕适配，以及组件库引入的准备工作

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '@/utils/vant-ui'
+// 按需导入组件库
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-const routes = [
-]
-
 const router = new VueRouter({
-  routes
+  routes: []
 })
 
 export default router
